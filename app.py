8b9a795f8ab74abc8fa52014126ea7c2385d2804
mkdemmers42 <mkdemmers42@gmail.com>
2026-05-17 18:47:59 -0700

    Update app.py

--- app.py
+++ app.py
@@ -571,7 +571,7 @@ elif can_run:
         data=summary_df.to_csv(index=False).encode("utf-8"),
         file_name="the_auditor_productivity_summary.csv",
         mime="text/csv",
-        )
+    )
 
     except Exception as exc:
         st.error("The Auditor hit an issue while reading the file.")
